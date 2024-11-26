654d001fc65fff430ccf4dd82f9b01588fb46982
Clarinhast04 <mariaclarasantana014@gmail.com>
2024-11-26 12:09:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'filme',
+    'Qual é a princesa mais jovem da Disney?',
+    ' Branca de neve'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'filme',
+    'Qual a tradução para a expressão "Hakuna Matata"?',
+    'Não se preocupe'
+)
+
+criaCartao(
+    'Serie',
+    'Qual personagem de Game of trones conseguiu se curar de Escamagris, com a ajuda de Sam Tarly?',
+    'Jorah Mormont'
+)
+
+criaCartao(
+    'Desenho',
+    'Onde Bob Esponja mora?',
+    'Em um abacaxi'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
